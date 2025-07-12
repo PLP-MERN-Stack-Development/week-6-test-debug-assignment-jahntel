c72cf4c36318bbc165945d8a9c1bcf0f6a2e166f
John Nzau <johnnzau845@gmail.com>
2025-07-12 12:57:38 +0300

    Bug.js

---
+++ Bug.js
@@ -0,0 +1,33 @@
+
+const mongoose = require('mongoose');
+
+const bugSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please add a title'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add a description'],
+    },
+    status: {
+      type: String,
+      enum: ['open', 'in-progress', 'resolved'],
+      default: 'open',
+    },
+    priority: {
+      type: String,
+      enum: ['low', 'medium', 'high'],
+      default: 'medium',
+    },
+    assignedTo: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Bug', bugSchema);
